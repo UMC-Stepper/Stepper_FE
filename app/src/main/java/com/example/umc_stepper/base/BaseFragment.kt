25d9efc9e2180ff9bca27494e87161b559123d10
Seongmin <arcn1345@gmail.com>
2024-07-07 19:57:10 +0900

    feat: first commit

---
+++ app/src/main/java/com/example/umc_stepper/base/BaseFragment.kt
@@ -0,0 +1,36 @@
+package com.example.umc_stepper.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding>(@LayoutRes private val layoutRes: Int) : Fragment() {
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setLayout()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    abstract fun setLayout()
+}
---
+++ app/src/main/java/com/example/umc_stepper/utils/GlobalApplication.kt
@@ -0,0 +1,7 @@
+package com.example.umc_stepper.utils
+
+import android.app.Application
+
+class GlobalApplication: Application() {
+
+}
