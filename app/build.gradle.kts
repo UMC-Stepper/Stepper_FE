03203ea46b3b248b2b0f2f2d11c316be6569b558
Seongmin <arcn1345@gmail.com>
2024-07-08 16:16:17 +0900

    feat: okhttp gradle 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
 
     // okHttp
     implementation(libs.okhttp)
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.okhttp3:okhttp-urlconnection:4.12.0")
 
     // retrofit
     implementation(libs.retrofit)
