4ffce0edc050acef074a9718b73ceb67a8925d45
김성민 <arcn1345@gmail.com>
2024-07-17 00:00:28 +0900

    feat: BaseAdapter, BaseDiffCallback 추가

---
+++ app/src/main/java/com/example/umc_stepper/base/BaseDiffCallback.kt
@@ -0,0 +1,17 @@
+package com.example.umc_stepper.base
+
+import androidx.recyclerview.widget.DiffUtil
+
+class BaseDiffCallback<T : Any>(
+    private val areItemsTheSame: (oldItem: T, newItem: T) -> Boolean,
+    private val areContentsTheSame: (oldItem: T, newItem: T) -> Boolean
+) : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+        return areItemsTheSame(oldItem, newItem)
+    }
+
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+        return areContentsTheSame(oldItem, newItem)
+    }
+
+}
