3cc019644a98247b539b366a08bc94cf8d87c11c
Seongmin <arcn1345@gmail.com>
2024-07-17 16:47:15 +0900

    feat: 바텀 네비, nav host 프래그먼트 추가

--- app/src/main/java/com/example/umc_stepper/ui/MainActivity.kt
+++ app/src/main/java/com/example/umc_stepper/ui/MainActivity.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseActivity
+import com.example.umc_stepper.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun setLayout() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setNavigation() {
+
     }
 }
