d0d7338466e00aa06215264d8017e25d83216d35
Seongmin <arcn1345@gmail.com>
2024-07-17 16:20:25 +0900

    feat: 프래그먼트 추가

---
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/StepperFragment.kt
@@ -0,0 +1,25 @@
+package com.example.umc_stepper.ui.stepper
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.umc_stepper.R
+
+class StepperFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stepper, container, false)
+    }
+
+}
