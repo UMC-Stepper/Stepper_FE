da5ff8d4ad9977456c4c2c863b558d5858fab0af
Seongmin <arcn1345@gmail.com>
2024-07-17 17:24:42 +0900

    feat: 메인액비비티 - 바텀네비 연결

--- app/src/main/java/com/example/umc_stepper/ui/MainActivity.kt
+++ app/src/main/java/com/example/umc_stepper/ui/MainActivity.kt
@@ -5,17 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseActivity
 import com.example.umc_stepper.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
+    private lateinit var navController: NavController
+
     override fun setLayout() {
-        TODO("Not yet implemented")
+        setNavigation()
     }
 
     private fun setNavigation() {
 
+        binding.mainBottomNavigationBar.itemIconTintList = null
+
+        val host = supportFragmentManager
+            .findFragmentById(binding.mainNavHostFragment.id) as NavHostFragment ?: return
+        navController = host.navController
+        binding.mainBottomNavigationBar.setupWithNavController(navController)
+
+        // 최소 실행시 프래그먼트 설정
+        binding.mainBottomNavigationBar.selectedItemId = R.id.todayFragment
+        navController.navigate(R.id.todayFragment)
     }
 }
