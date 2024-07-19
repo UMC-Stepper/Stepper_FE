8b4a687f122e981f6c263f8fd5bbf78d09f4f2c0
Lee Chaeyoung <0929alice@naver.com>
2024-07-20 00:02:59 +0900

    FEAT: 로그인 액티비티 추가

--- app/src/main/java/com/example/umc_stepper/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/umc_stepper/ui/login/LoginActivity.kt
@@ -1,4 +1,22 @@
 package com.example.umc_stepper.ui.login
 
-class LoginActivity {
+import android.content.Intent
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseActivity
+import com.example.umc_stepper.databinding.ActivityLoginBinding
+import com.example.umc_stepper.databinding.ActivityMainBinding
+import com.example.umc_stepper.ui.MainActivity
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login){
+    override fun setLayout() {
+        goHome()
+    }
+
+    private fun goHome(){
+        binding.activityLoginStepperIv.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
+
