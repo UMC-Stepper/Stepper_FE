750e42d7a86aa7083d115be50bad87e01a8f6118
Lee Chaeyoung <0929alice@naver.com>
2024-07-20 15:20:10 +0900

    FEAT: 로그인 액티비티 상단바 투명

--- app/src/main/java/com/example/umc_stepper/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/umc_stepper/ui/login/LoginActivity.kt
@@ -1,15 +1,16 @@
 package com.example.umc_stepper.ui.login
 
 import android.content.Intent
+import android.view.WindowManager
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseActivity
 import com.example.umc_stepper.databinding.ActivityLoginBinding
-import com.example.umc_stepper.databinding.ActivityMainBinding
 import com.example.umc_stepper.ui.MainActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login){
     override fun setLayout() {
         goHome()
+        barTransparent()
     }
 
     private fun goHome(){
@@ -17,6 +18,12 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun barTransparent(){
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
 
