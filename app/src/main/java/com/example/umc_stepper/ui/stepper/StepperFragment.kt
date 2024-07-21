40248462bb22de427816250ce88bb632bf73ff0f
Lee Chaeyoung <0929alice@naver.com>
2024-07-21 21:06:45 +0900

    FEAT: 스테퍼홈 캘린더 중간점검

--- app/src/main/java/com/example/umc_stepper/ui/stepper/StepperFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/StepperFragment.kt
@@ -1,25 +1,31 @@
 package com.example.umc_stepper.ui.stepper
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseFragment
+import com.example.umc_stepper.databinding.FragmentStepperBinding
+import com.example.umc_stepper.ui.stepper.home.CalendarAdapter
+import com.example.umc_stepper.ui.stepper.home.DayData
 
-class StepperFragment : Fragment() {
+class StepperFragment : BaseFragment<FragmentStepperBinding>(R.layout.fragment_stepper) {
+    override fun setLayout() {
+        val days = listOf(
+            DayData("월", false, false), DayData("화", false, false), DayData("수", false, false),
+            DayData("목", false, false), DayData("금", false, false), DayData("토", false, false), DayData("일", false, false),
+            DayData("29", false, false), DayData("30", false, false), DayData("31", false, false),
+            DayData("1", false, false), DayData("2", false, false), DayData("3", false, false), DayData("4", false, false),
+            DayData("5", false, false), DayData("6", false, false), DayData("7", false, false), DayData("8", false, false),
+            DayData("9", true, false), DayData("10", false, false), DayData("11", false, false),
+            DayData("12", false, false), DayData("13", false, false), DayData("14", false, false), DayData("15", false, false),
+            DayData("16", false, true), DayData("17", false, false), DayData("18", false, false),
+            DayData("19", false, false), DayData("20", false, false), DayData("21", false, false), DayData("22", false, false),
+            DayData("23", false, false), DayData("24", false, false), DayData("25", false, false),
+            DayData("26", true, false), DayData("27", false, false), DayData("28", false, false), DayData("29", false, false),
+            DayData("30", false, false), DayData("31", false, false), DayData("1", false, false)
+        )
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+        val adapter = CalendarAdapter(this, days)
+        binding.stepperCalendarGv.adapter = adapter
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stepper, container, false)
-    }
 
 }
