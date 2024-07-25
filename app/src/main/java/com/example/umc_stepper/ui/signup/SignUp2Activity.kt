dc36aad307e4d5edacc48ea9838a15d101c04672
jiwon <pjw99032986@gmail.com>
2024-07-25 17:25:29 +0900

    refactor : 숫자 입력 시 실패 검증 로직 추가

--- app/src/main/java/com/example/umc_stepper/ui/signup/SignUp2Activity.kt
+++ app/src/main/java/com/example/umc_stepper/ui/signup/SignUp2Activity.kt
@@ -1,15 +1,21 @@
 package com.example.umc_stepper.ui.signup
 
 import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseActivity
 import com.example.umc_stepper.databinding.FragmentRegister2Binding
 import com.example.umc_stepper.domain.model.UserDto
+import com.example.umc_stepper.ui.MainActivity
 import com.example.umc_stepper.utils.GlobalApplication
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
@@ -23,10 +29,11 @@ class SignUp2Activity : BaseActivity<FragmentRegister2Binding>(R.layout.fragment
         init()
     }
 
-    private fun init(){
+    private fun init() {
         initActivityResultLauncher()
         receiveUserData()
         setOnClickBtn()
+        addTextWatcher()
     }
 
     //갤러리 런처 초기화
@@ -37,7 +44,10 @@ class SignUp2Activity : BaseActivity<FragmentRegister2Binding>(R.layout.fragment
             if (result.resultCode == RESULT_OK) {
                 val selectImageUrl = result.data?.data
                 selectImageUrl?.let {
-                    GlobalApplication.loadProfileImage(binding.fragmentRegister2ProfileFrameIv, it.toString())
+                    GlobalApplication.loadProfileImage(
+                        binding.fragmentRegister2ProfileFrameIv,
+                        it.toString()
+                    )
                     binding.fragmentRegister2ProfileImgIv.visibility = View.GONE
                     user.profile_image = it.toString()
                 }
@@ -65,8 +75,14 @@ class SignUp2Activity : BaseActivity<FragmentRegister2Binding>(R.layout.fragment
                 // 성공 버튼 클릭 시 동작
                 val height = fragmentRegister2HeightEt.text.toString()
                 val weight = fragmentRegister2WeightEt.text.toString()
-                user.weight = weight
-                user.height = height
+                confirmNumberText(height,fragmentRegister2HeightEt)
+                confirmNumberText(weight,fragmentRegister2WeightEt)
+
+                if(weight.isNotEmpty() && height.isNotEmpty()) {
+                    user.weight = weight
+                    user.height = height
+                    startActivity(Intent(this@SignUp2Activity,MainActivity::class.java))
+                }
                 //viewModel.provideUserDto...(서버로 userDto 담아서 보내기)
 
             }
@@ -76,6 +92,15 @@ class SignUp2Activity : BaseActivity<FragmentRegister2Binding>(R.layout.fragment
         }
     }
 
+
+    private fun confirmNumberText(confirmText: String, view: EditText) {
+        try {
+            confirmText.toInt()
+        } catch (e: NumberFormatException) {
+            view.setText("")
+            Toast.makeText(this@SignUp2Activity,"숫자만 입력해주세요.",Toast.LENGTH_SHORT).show()
+        }
+    }
     //갤러리 열기
     private fun openGallery() {
         val galleryIntent = Intent()
@@ -83,4 +108,58 @@ class SignUp2Activity : BaseActivity<FragmentRegister2Binding>(R.layout.fragment
             .setAction(Intent.ACTION_GET_CONTENT)
         galleryForResult.launch(Intent.createChooser(galleryIntent, "Select Picture"))
     }
+
+    private fun addTextWatcher() {
+        binding.fragmentRegister2WeightEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkWeightField()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.fragmentRegister2HeightEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkHeightField()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun checkWeightField() {
+        val wTextField = binding.fragmentRegister2WeightEt.text.toString()
+        val drawableOk = ContextCompat.getDrawable(this, R.drawable.ic_signup_pwd_check_ok)?.apply {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+        }
+        val drawableError =
+            ContextCompat.getDrawable(this, R.drawable.ic_signup_pwd_check_error)?.apply {
+                setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            }
+
+        if (wTextField.isNotEmpty()) {
+            binding.fragmentRegister2WeightEt.setCompoundDrawables(null, null, drawableOk, null)
+        } else {
+            binding.fragmentRegister2WeightEt.setCompoundDrawables(null, null, drawableError, null)
+        }
+    }
+
+    private fun checkHeightField() {
+        val hTextField = binding.fragmentRegister2HeightEt.text.toString()
+        val drawableOk = ContextCompat.getDrawable(this, R.drawable.ic_signup_pwd_check_ok)?.apply {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+        }
+        val drawableError =
+            ContextCompat.getDrawable(this, R.drawable.ic_signup_pwd_check_error)?.apply {
+                setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            }
+
+        if (hTextField.isNotEmpty()) {
+            binding.fragmentRegister2HeightEt.setCompoundDrawables(null, null, drawableOk, null)
+        } else {
+            binding.fragmentRegister2HeightEt.setCompoundDrawables(null, null, drawableError, null)
+        }
+    }
+
 }
