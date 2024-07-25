e522ccae622d631496a132007cfbebedf6562ad0
jiwon <pjw99032986@gmail.com>
2024-07-25 16:38:05 +0900

    refactor : 글라이드 모듈 추가

--- app/src/main/java/com/example/umc_stepper/utils/GlobalApplication.kt
+++ app/src/main/java/com/example/umc_stepper/utils/GlobalApplication.kt
@@ -1,7 +1,33 @@
 package com.example.umc_stepper.utils
 
 import android.app.Application
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
 
-class GlobalApplication: Application() {
+class GlobalApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: GlobalApplication
+            private set
+
+        fun loadImage(imageView: ImageView, source: Any) {
+            Glide.with(instance)
+                .load(source)
+                .into(imageView)
+        }
+
+        fun loadProfileImage(imageView: ImageView, source: Any) {
+            Glide.with(instance)
+                .load(source)
+                .apply(RequestOptions.bitmapTransform(CircleCrop()))
+                .into(imageView)
+        }
+    }
 }
