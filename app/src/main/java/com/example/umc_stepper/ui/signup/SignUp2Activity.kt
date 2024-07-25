35eb62fe8e9cd580379d06bb7d256950454266a7
jiwon <pjw99032986@gmail.com>
2024-07-25 15:11:01 +0900

    feat : 로그인 2 페이지 추가

---
+++ app/src/main/java/com/example/umc_stepper/ui/signup/SignUp2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.umc_stepper.ui.signup
+
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseActivity
+import com.example.umc_stepper.databinding.FragmentRegister2Binding
+
+class SignUp2Activity : BaseActivity<FragmentRegister2Binding>(R.layout.fragment_register_2) {
+    override fun setLayout() {
+
+    }
+}
