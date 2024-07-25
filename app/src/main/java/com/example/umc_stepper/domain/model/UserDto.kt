b8efedbddffbe01a620d10c4c11c6ba3c2a9e8f7
jiwon <pjw99032986@gmail.com>
2024-07-25 15:12:59 +0900

    feat : 회원 가입을 위한 useDto 정의

---
+++ app/src/main/java/com/example/umc_stepper/domain/model/UserDto.kt
@@ -0,0 +1,15 @@
+package com.example.umc_stepper.domain.model
+
+data class UserDto(
+    val community_alarm: String = "",
+    val email: String = "",
+    val email_agree: String = "",
+    val exercise_alarm: String = "",
+    val height: String = "",
+    val nick_name: String = "",
+    val password: String = "",
+    val per_agree: String = "",
+    val profile_image: String= "",
+    val use_agree: String = "",
+    val weight: String= ""
+)
