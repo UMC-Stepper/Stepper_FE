7aa18339156df3f0af4ced60057c06e006929b5c
김성민 <arcn1345@gmail.com>
2024-07-30 15:31:02 +0900

    feat: 스태퍼 - 스크랩 프래그먼트 layout 파일 이름 변경

--- app/src/main/java/com/example/umc_stepper/ui/stepper/additional/AddExerciseDownloadFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/additional/AddExerciseDownloadFragment.kt
@@ -3,9 +3,9 @@ package com.example.umc_stepper.ui.stepper.additional
 import android.view.WindowManager
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
-import com.example.umc_stepper.databinding.FragmentAddExerciseDownloadBinding
+import com.example.umc_stepper.databinding.FragmentAddExerciseSelectScrapBinding
 
-class AddExerciseDownloadFragment : BaseFragment<FragmentAddExerciseDownloadBinding>(R.layout.fragment_add_exercise_select_scrap){
+class AddExerciseDownloadFragment : BaseFragment<FragmentAddExerciseSelectScrapBinding>(R.layout.fragment_add_exercise_select_scrap){
     override fun setLayout() {
         barTransparent()
     }
