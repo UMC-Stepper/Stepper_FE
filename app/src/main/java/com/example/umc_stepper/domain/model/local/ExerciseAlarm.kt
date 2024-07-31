af6f2fe78aac06553d02f6e883b66133925ed2c7
김성민 <arcn1345@gmail.com>
2024-07-31 16:08:42 +0900

    feat: ExerciseAlarm 데이터 클래스 추가

---
+++ app/src/main/java/com/example/umc_stepper/domain/model/local/ExerciseAlarm.kt
@@ -0,0 +1,8 @@
+package com.example.umc_stepper.domain.model.local
+
+data class ExerciseAlarm (
+    val day: String,
+    val time: String,
+    val amPm: String,
+    val materials: String? = null
+)
