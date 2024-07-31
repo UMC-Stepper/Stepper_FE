e516392e76d9269ce4b8a3f849c52c3f374f06a5
jiwon <pjw99032986@gmail.com>
2024-07-31 17:14:17 +0900

    Revert "feat : today ViewModel AI 테스트, 주석해제"
    This reverts commit ecc6873c63f31c93075a16da519f9c56604baa2d.

--- app/src/main/java/com/example/umc_stepper/ui/today/TodayViewModel.kt
+++ app/src/main/java/com/example/umc_stepper/ui/today/TodayViewModel.kt
@@ -9,7 +9,7 @@ import com.example.umc_stepper.domain.model.response.Ylist
 import com.example.umc_stepper.domain.model.response.YouTubeVideo
 import com.example.umc_stepper.domain.repository.FastApiRepository
 import com.example.umc_stepper.domain.repository.YoutubeApiRepository
-import com.example.umc_stepper.BuildConfig
+import com.pierfrancescosoffritti.androidyoutubeplayer.BuildConfig
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -34,7 +34,7 @@ class TodayViewModel @Inject constructor(
     val successYoutubeLink: StateFlow<Ylist> = _successYoutubeLink
 
 
-    val youtubeKey = BuildConfig.YOUTUBE_KEY
+    //val youtubeKey = BuildConfig.YOUTUBE_KEY
 
     fun postAiVideoInfo(aiVideoDto: AiVideoDto) {
         viewModelScope.launch {
@@ -73,8 +73,8 @@ class TodayViewModel @Inject constructor(
 
     fun getYoutubeVideoInfoSequentially(firstUrl: String, secondUrl: String) {
         viewModelScope.launch {
-            getYoutubeVideoInfo("snippet", firstUrl, youtubeKey)
-            getYoutubeVideoInfo("snippet", secondUrl, youtubeKey)
+            //getYoutubeVideoInfo("snippet", firstUrl, youtubeKey)
+            //getYoutubeVideoInfo("snippet", secondUrl, youtubeKey)
         }
     }
 }
