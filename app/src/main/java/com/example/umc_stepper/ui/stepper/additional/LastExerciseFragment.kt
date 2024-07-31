eca11fa93c9aba53a2d14df3dd47bc3a0307b67c
Lee Chaeyoung <0929alice@naver.com>
2024-07-31 22:06:28 +0900

    feat: 추가운동(유튜브+타이머)부분에 타이머기능 합치기

---
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/additional/LastExerciseFragment.kt
@@ -0,0 +1,77 @@
+package com.example.umc_stepper.ui.stepper.additional
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Button
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseFragment
+import com.example.umc_stepper.databinding.FragmentLastExerciseBinding
+
+class LastExerciseFragment : BaseFragment<FragmentLastExerciseBinding>(R.layout.fragment_last_exercise) {
+
+    private var isRunning = false
+    private var time = 0L // 밀리초 단위
+
+    private lateinit var startBtn: Button
+    private lateinit var resetBtn: Button
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val timerRunnable = object : Runnable {
+        override fun run() {
+            if (isRunning) {
+                time += 1000
+                updateTimerUI(time)
+                handler.postDelayed(this, 1000)
+            }
+        }
+    }
+
+    override fun setLayout() {
+        initButton()
+        setTimer()
+        resetTimer()
+    }
+
+    private fun initButton() {
+        startBtn = binding.fragmentAdditionalMiddleStartBtn
+        resetBtn = binding.fragmentAdditionalMiddleResetBtn
+    }
+
+    private fun setTimer() {
+        startBtn.setOnClickListener {
+            if (!isRunning) {
+                isRunning = true
+                startBtn.text = "중지"
+                handler.post(timerRunnable) // 타이머 시작
+            } else {
+                isRunning = false
+                startBtn.text = "계속"
+                handler.removeCallbacks(timerRunnable) // 타이머 중지
+            }
+        }
+    }
+
+    private fun updateTimerUI(time: Long) {
+        val timeInSeconds = time / 1000
+        val hours = timeInSeconds / 3600
+        val minutes = (timeInSeconds % 3600) / 60
+        val seconds = timeInSeconds % 60
+
+        Log.d("updateTimerUI", "시: $hours 분: $minutes 초: $seconds")
+
+        binding.fragmentAdditionalMiddleHourTv.text = String.format("%02d", hours)
+        binding.fragmentAdditionalMiddleMinTv.text = String.format("%02d", minutes)
+        binding.fragmentAdditionalMiddleSecTv.text = String.format("%02d", seconds)
+    }
+
+    private fun resetTimer() {
+        resetBtn.setOnClickListener {
+            isRunning = false
+            time = 0
+            startBtn.text = "시작"
+            updateTimerUI(time)
+            handler.removeCallbacks(timerRunnable) // 타이머 중지
+        }
+    }
+}
