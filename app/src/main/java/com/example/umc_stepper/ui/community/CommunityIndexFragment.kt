1b90936e73ca409b2c3f720ce04b180c30312e44
Lee Chaeyoung <0929alice@naver.com>
2024-08-03 23:37:34 +0900

    refactor: 버튼 사항 변경

--- app/src/main/java/com/example/umc_stepper/ui/community/CommunityIndexFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/community/CommunityIndexFragment.kt
@@ -8,12 +8,11 @@ import com.example.umc_stepper.databinding.FragmentCommunityIndexBinding
 
 class CommunityIndexFragment :BaseFragment<FragmentCommunityIndexBinding>(R.layout.fragment_community_index) {
     override fun setLayout() {
-        //임시함수적용: 추후 아래의 goCommunityIndex()함수와 함께 꼭 삭제
         binding.commnunityIndexPostBtn.setOnClickListener {
             goCommunityIndexPost()
         }
     }
-    //임시함수(꼭 추후삭제) 작성글목록확인으로 이동
+
     private fun goCommunityIndexPost(){
         findNavController().navigate(R.id.action_communityIndexFragment_to_communityIndexPostFragment)
     }
