5bb4513f89ba986dc3efe902431eca9a42687051
김성민 <arcn1345@gmail.com>
2024-08-03 19:03:16 +0900

    feat: 커뮤니티 위클리 홈 프래그먼트 추가

---
+++ app/src/main/java/com/example/umc_stepper/ui/community/weekly/CommunityWeeklyHomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.umc_stepper.ui.community.weekly
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseFragment
+import com.example.umc_stepper.databinding.FragmentCommunityWeeklyHomeBinding
+import com.example.umc_stepper.ui.MainActivity
+
+class CommunityWeeklyHomeFragment : BaseFragment<FragmentCommunityWeeklyHomeBinding>(R.layout.fragment_community_weekly_home) {
+
+    private lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    override fun setLayout() {
+        updateMainToolbar()
+    }
+
+    private fun updateMainToolbar() {
+        mainActivity.updateToolbarTitle("Weekly Mission")
+        mainActivity.updateToolbarLeftImg(R.drawable.ic_toolbar_community_home)
+        mainActivity.updateToolbarMiddleImg(R.drawable.ic_toolbar_community_search)
+        mainActivity.updateToolbarRightImg(R.drawable.ic_toolbar_community_menu)
+    }
+
+}
