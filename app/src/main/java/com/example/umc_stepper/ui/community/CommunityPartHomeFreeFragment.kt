d4b0cea4055379ed4a39b6f823c2da5a6df58f76
Lee Chaeyoung <0929alice@naver.com>
2024-08-04 17:12:48 +0900

    feat :  부위별 게시판 탭 레이아웃 연결

---
+++ app/src/main/java/com/example/umc_stepper/ui/community/CommunityPartHomeFreeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.umc_stepper.ui.community
+
+import android.view.WindowManager
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseFragment
+import com.example.umc_stepper.databinding.FragmentCommunityPartHomeTabBinding
+
+class CommunityPartHomeFreeFragment :BaseFragment<FragmentCommunityPartHomeTabBinding>(R.layout.fragment_community_part_home_tab) {
+    override fun setLayout() {
+        binding.communityPartHomeTabTv.text="자유 Talk 글이 없습니다"
+    }
+
+}
