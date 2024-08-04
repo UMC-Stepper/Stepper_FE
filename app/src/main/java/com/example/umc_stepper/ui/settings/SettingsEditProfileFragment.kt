e2b1c47f6872c7e420cf0fbac32ad6c02cc9e90d
김성민 <arcn1345@gmail.com>
2024-08-05 03:02:14 +0900

    feat: 셋팅 개인 정보 수정 화면 추가

---
+++ app/src/main/java/com/example/umc_stepper/ui/settings/SettingsEditProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.example.umc_stepper.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseFragment
+import com.example.umc_stepper.databinding.FragmentSettingsEditProfileBinding
+
+class SettingsEditProfileFragment : BaseFragment<FragmentSettingsEditProfileBinding>(R.layout.fragment_settings_edit_profile) {
+
+    override fun setLayout() {
+        
+    }
+
+}
