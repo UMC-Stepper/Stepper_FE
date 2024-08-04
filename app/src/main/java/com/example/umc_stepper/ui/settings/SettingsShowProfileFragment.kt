4c336687b2e94a0e55e6bf8a02ee09a80f364d43
김성민 <arcn1345@gmail.com>
2024-08-05 03:38:30 +0900

    feat: SettingsShowProfileFragment 버튼 클릭시 화면 이동

--- app/src/main/java/com/example/umc_stepper/ui/settings/SettingsShowProfileFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/settings/SettingsShowProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
 import com.example.umc_stepper.databinding.FragmentSettingsShowProfileBinding
@@ -12,7 +13,14 @@ import com.example.umc_stepper.databinding.FragmentSettingsShowProfileBinding
 class SettingsShowProfileFragment : BaseFragment<FragmentSettingsShowProfileBinding>(R.layout.fragment_settings_show_profile) {
 
     override fun setLayout() {
+        setButton()
+    }
 
+    private fun setButton() {
+        binding.fragmentSettingsShowEditBtn.setOnClickListener {
+            val action = SettingsHomeFragmentDirections.actionSettingsFragmentToSettingsShowProfileFragment()
+            findNavController().navigate(action.actionId)
+        }
     }
 
 }
