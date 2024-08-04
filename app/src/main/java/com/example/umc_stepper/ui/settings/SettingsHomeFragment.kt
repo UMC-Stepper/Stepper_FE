224f585f400997d8502f33f5491012022fc94a0e
김성민 <arcn1345@gmail.com>
2024-08-05 02:59:20 +0900

    reactor: fragment_settings -> fragment_settings_home 으로 이름 변경

--- app/src/main/java/com/example/umc_stepper/ui/settings/SettingsHomeFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/settings/SettingsHomeFragment.kt
@@ -20,7 +20,7 @@ class SettingsHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_settings_home, container, false)
     }
 
 }
