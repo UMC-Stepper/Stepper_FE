cf44d8ea4352486036429b5ff693fefc776c2d0b
Lee Chaeyoung <0929alice@naver.com>
2024-08-04 16:28:09 +0900

    feat :  커뮤니티-댓글모음,스크랩모음 버튼연결 + 목록툴바 전체 추가

--- app/src/main/java/com/example/umc_stepper/ui/community/CommunityIndexFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/community/CommunityIndexFragment.kt
@@ -1,13 +1,23 @@
 package com.example.umc_stepper.ui.community
 
+import android.content.Context
 import android.view.WindowManager
 import androidx.navigation.fragment.findNavController
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
 import com.example.umc_stepper.databinding.FragmentCommunityIndexBinding
+import com.example.umc_stepper.ui.MainActivity
 
 class CommunityIndexFragment :BaseFragment<FragmentCommunityIndexBinding>(R.layout.fragment_community_index) {
+    private lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
     override fun setLayout() {
+        updateToolbar()
         binding.commnunityIndexPostBtn.setOnClickListener {
             goCommunityIndexPost()
         }
@@ -20,16 +30,22 @@ class CommunityIndexFragment :BaseFragment<FragmentCommunityIndexBinding>(R.layo
             goCommunityIndexScrap()
         }
     }
+    private fun updateToolbar() {
+        mainActivity.updateToolbarTitle("목록")
+        mainActivity.updateToolbarLeftImg(R.drawable.ic_toolbar_community_home)
+        mainActivity.updateToolbarMiddleImg(R.drawable.ic_toolbar_community_search)
+        mainActivity.updateToolbarRightImg(R.drawable.ic_toolbar_community_menu)
+    }
 
     private fun goCommunityIndexPost(){
         findNavController().navigate(R.id.action_communityIndexFragment_to_communityIndexPostFragment)
     }
 
     private fun goCommunityIndexComment(){
-        findNavController().navigate(R.id.action_communityIndexFragment_to_communityIndexCommentFragment)
+        findNavController().navigate(R.id.action_communityIndexFragment_to_communityMyCommentsFragment)
     }
 
     private fun goCommunityIndexScrap(){
-        findNavController().navigate(R.id.action_communityIndexFragment_to_communityIndexScrapFragment)
+        findNavController().navigate(R.id.action_communityIndexFragment_to_communityMyScrapFragment)
     }
 }
