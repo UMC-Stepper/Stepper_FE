04d873b188eae91ac28a427277d740dd04bc1f66
jiwon <pjw99032986@gmail.com>
2024-08-05 21:04:58 +0900

    feat : 검색 커뮤니티 아이템 더미데이터

---
+++ app/src/main/java/com/example/umc_stepper/domain/model/PostItem.kt
@@ -0,0 +1,11 @@
+package com.example.umc_stepper.domain.model
+
+
+data class PostItem(
+    val title : String,
+    val img : String,
+    val description : String,
+    val like : Int,
+    val scrap : Int,
+    val message : Int
+)
