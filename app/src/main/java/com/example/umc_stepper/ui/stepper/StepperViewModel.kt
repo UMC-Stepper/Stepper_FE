9bcf1a6157bfe7044d98845d4044cbe8e2f9e982
Lee Chaeyoung <0929alice@naver.com>
2024-08-07 21:31:02 +0900

    feat: 추가운동 유튜브api 중간저장

--- app/src/main/java/com/example/umc_stepper/ui/stepper/StepperViewModel.kt
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/StepperViewModel.kt
@@ -1,16 +1,27 @@
 package com.example.umc_stepper.ui.stepper
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.umc_stepper.domain.model.response.YouTubeVideo
+import com.example.umc_stepper.domain.repository.YoutubeApiRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.util.logging.Level
-
-class StepperViewModel : ViewModel() {
+import javax.inject.Inject
+@HiltViewModel
+class StepperViewModel @Inject constructor(
+    private val youtubeApiRepository: YoutubeApiRepository
+): ViewModel() {
     private val _levelItems = MutableLiveData<List<LevelListItem>>()
     val levelItems: LiveData<List<LevelListItem>> = _levelItems
+
+    private val _successYoutubeLink = MutableStateFlow(YouTubeVideo())
+    val provideYoutubeLink: StateFlow<YouTubeVideo> = _successYoutubeLink
 
     init {
         loadLevelItems()
@@ -41,5 +52,18 @@ class StepperViewModel : ViewModel() {
             )
         )
         _levelItems.value = items
+    }
+
+    fun getYoutubeVideoInfo(part: String, id: String, key: String) {
+        viewModelScope.launch {
+            try {
+                youtubeApiRepository.getYoutubeDetail(part, id, key).collect { video ->
+                    _successYoutubeLink.value=video
+
+                }
+            } catch (e: Exception) {
+                Log.e("Get YoutubeVideo is Error", e.message.toString())
+            }
+        }
     }
 }
--- app/src/main/java/com/example/umc_stepper/ui/stepper/additional/AdditionalExerciseYoutube2Fragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/additional/AdditionalExerciseYoutube2Fragment.kt
@@ -2,18 +2,29 @@ package com.example.umc_stepper.ui.stepper.additional
 
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.example.umc_stepper.BuildConfig
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
 import com.example.umc_stepper.databinding.FragmentAdditionalExerciseYoutube2Binding
+import com.example.umc_stepper.ui.stepper.StepperViewModel
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import java.net.URL
 import java.net.URLDecoder
-
+@AndroidEntryPoint
 class AdditionalExerciseYoutube2Fragment : BaseFragment<FragmentAdditionalExerciseYoutube2Binding>(R.layout.fragment_additional_exercise_youtube2) {
 
+    val stepperViewModel:StepperViewModel by activityViewModels()
+    val youtubeKey = BuildConfig.YOUTUBE_KEY
     override fun setLayout() {
         val urlText = arguments?.getString("urlText")
         if (!urlText.isNullOrEmpty()) {
@@ -30,7 +41,8 @@ class AdditionalExerciseYoutube2Fragment : BaseFragment<FragmentAdditionalExerci
     private fun initializeYouTubePlayer(url: String) {
         lifecycle.addObserver(binding.fragmentDownloadYoutube2MainCardPreviewYv)
 
-        binding.fragmentDownloadYoutube2MainCardPreviewYv.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+        binding.fragmentDownloadYoutube2MainCardPreviewYv.addYouTubePlayerListener(object :
+            AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 val videoId = extractVideoId(url)
                 youTubePlayer.loadVideo(videoId, 0f)
@@ -59,21 +71,33 @@ class AdditionalExerciseYoutube2Fragment : BaseFragment<FragmentAdditionalExerci
         val videoId = extractVideoId(url)
         // videoId를 사용하여 비디오 세부 정보를 가져오는 선호하는 방법 사용 (예: Retrofit, OkHttp 등)
         // 데이터를 가져오기 위한 플레이스홀더 함수
-        fetchVideoDetailsFromApi(videoId) { title, channelName, channelProfileUrl ->
-            binding.fragmentDownloadYoutube2MainCardChannelNameTv.text = title
-            binding.fragmentDownloadYoutube2ProfileNameTv.text = channelName
-            // 이미지 로딩 라이브러리를 사용하여 ImageView에 이미지 로드 (예: Glide, Picasso)
-            Glide.with(this)
-                .load(channelProfileUrl)
-                .into(binding.fragmentDownloadYoutube2MainCardChannelProfileIv)
-        }
+        fetchVideoDetailsFromApi("snippet",videoId,youtubeKey)
     }
 
     //유튜브api에서 세부정보 불러오는 함수..
-    private fun fetchVideoDetailsFromApi(videoId: String, callback: (String, String, String) -> Unit) {
-        //일단 불러왔다고 가정하고 임시데이터에요!
+    private fun fetchVideoDetailsFromApi(part: String, id: String, key: String) {
+        stepperViewModel.getYoutubeVideoInfo(part,id,key)
 
-        callback("윗몸일으키기 제대로 하는 방법", "비타밍제이", "https://yt3.ggpht.com/l0AxbcHO4TRBQFka9rUZpiM19BQxueUZ_UE4wHW8qwaLZtZ_3J4fIXmay5HurJH03LJ7cGirxFY=s88-c-k-c0x00ffffff-no-rj")
+
+    }
+
+    private fun dataSetting(){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                try{
+                    stepperViewModel.provideYoutubeLink.collect{
+                        binding.fragmentDownloadYoutube2MainCardChannelNameTv.text = it.items[0].snippet.title
+                        binding.fragmentDownloadYoutube2ProfileNameTv.text = it.items[0].snippet.channelTitle
+                        // 이미지 로딩 라이브러리를 사용하여 ImageView에 이미지 로드 (예: Glide, Picasso)
+                        Glide.with(requireContext())
+                            .load(it.items[0].snippet.thumbnails.default)
+                            .into(binding.fragmentDownloadYoutube2MainCardChannelProfileIv)
+                    }
+                }catch(e: Exception) {
+                }
+            }
+        }
+
     }
 
     private fun goLastExercise() {
@@ -83,4 +107,5 @@ class AdditionalExerciseYoutube2Fragment : BaseFragment<FragmentAdditionalExerci
         }
         findNavController().navigate(R.id.action_fragmentAdditionalExerciseYoutube2_to_fragmentLastExercise, bundle)
     }
+
 }
