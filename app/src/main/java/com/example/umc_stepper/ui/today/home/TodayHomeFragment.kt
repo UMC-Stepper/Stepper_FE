642fe982ecf15c558233c3cbce98f114142077d5
김성민 <arcn1345@gmail.com>
2024-08-07 23:41:23 +0900

    feat: 투데이 홈 운동카드 API 연결

--- app/src/main/java/com/example/umc_stepper/ui/today/home/TodayHomeFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/today/home/TodayHomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.umc_stepper.ui.today.home
 
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -26,6 +27,7 @@ import java.util.Locale
 class TodayHomeFragment : BaseFragment<FragmentTodayHomeBinding>(R.layout.fragment_today_home) {
 
     private lateinit var todayHomeCalendarAdapter: TodayHomeCalendarAdapter
+    private lateinit var todayHomeExerciseCardAdapter: TodayHomeExerciseCardAdapter
     private val todayViewModel : TodayViewModel by activityViewModels()
     private var calendarList = ArrayList<WeekCalendar>()
 
@@ -44,7 +46,30 @@ class TodayHomeFragment : BaseFragment<FragmentTodayHomeBinding>(R.layout.fragme
 
     // 첫 접속시 오늘 날짜로 운동카드 설정됨
     private fun firstConnect() {
+        val currentDate = LocalDate.now()
+        val selectedDate = LocalDate.of(currentDate.year, currentDate.monthValue, currentDate.dayOfMonth)
+        val formattedDate = selectedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            todayViewModel.getTodayExerciseState(formattedDate)
+            //updateUIVisibility()
+        }
+    }
+
+    private fun updateUIVisibility() {
+        val exerciseStateList = todayViewModel.exerciseState.value
+        var isSuccess = exerciseStateList?.firstOrNull()?.isSuccess ?: false
+
+        if (isSuccess) {
+            Log.d("로그", "isSuccess true : $isSuccess")
+            binding.fragmentTodayHomeExerciseCardRv.visibility = View.VISIBLE
+            binding.fragmentTodayHomeNoCardTv.visibility = View.INVISIBLE
+            todayHomeExerciseCardAdapter.submitList(exerciseStateList)
+        } else {
+            Log.d("로그", "isSuccess false : $isSuccess")
+            binding.fragmentTodayHomeExerciseCardRv.visibility = View.INVISIBLE
+            binding.fragmentTodayHomeNoCardTv.visibility = View.VISIBLE
+        }
     }
 
     // 툴바 달 설정
@@ -57,29 +82,34 @@ class TodayHomeFragment : BaseFragment<FragmentTodayHomeBinding>(R.layout.fragme
     private fun observeViewModel() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                todayViewModel.toDayExerciseResponseDto.collect { toDayExerciseResponseDto ->
-                    toDayExerciseResponseDto.result?.bodyPart
+                todayViewModel.exerciseState.collect {
+                    Log.d("로그", "Received exerciseState: ${todayViewModel.exerciseState.value}")
+                    updateUIVisibility()
                 }
             }
         }
     }
 
-    // 어댑터 날짜 초기화
+    // 어댑터 초기화 및 날짜 설정
     private fun initAdapter() {
+
+        todayHomeExerciseCardAdapter = TodayHomeExerciseCardAdapter()
+        binding.fragmentTodayHomeExerciseCardRv.adapter = todayHomeExerciseCardAdapter
+
+        // 어댑터 초기화 및 클릭 리스너 설정 (날짜 바뀌면서 운동카드 갱신)
+        todayHomeCalendarAdapter = TodayHomeCalendarAdapter { formattedDate ->
+            viewLifecycleOwner.lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    todayViewModel.getTodayExerciseState(formattedDate)
+                    //updateUIVisibility()
+                }
+            }
+        }
 
         var weekDay = resources.getStringArray(R.array.calendar_day_eng)
         val dateFormat = DateTimeFormatter.ofPattern("dd").withLocale(Locale.forLanguageTag("ko"))
         var preSunday: LocalDateTime = LocalDateTime.now().with(TemporalAdjusters.previous(DayOfWeek.SUNDAY))
         Log.d("weekDay", "weekDay : $weekDay, preSunday: $preSunday")
-
-        // 어댑터 초기화 및 클릭 리스너 설정
-        todayHomeCalendarAdapter = TodayHomeCalendarAdapter { formattedDate ->
-            viewLifecycleOwner.lifecycleScope.launch {
-                repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    todayViewModel.getTodayExerciseState(formattedDate)
-                }
-            }
-        }
 
         for (i in 0..6) {
             calendarList.apply {
