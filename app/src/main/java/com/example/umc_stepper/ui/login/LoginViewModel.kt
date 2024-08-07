76961106ff8f874687fea4dd616850c1f3024550
jiwon <pjw99032986@gmail.com>
2024-08-07 13:23:02 +0900

    feat : 로그인 연결

--- app/src/main/java/com/example/umc_stepper/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/umc_stepper/ui/login/LoginViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.umc_stepper.base.BaseResponse
+import com.example.umc_stepper.domain.model.request.LogInDto
 import com.example.umc_stepper.domain.model.request.UserDto
 import com.example.umc_stepper.domain.model.response.UserResponse
 import com.example.umc_stepper.domain.repository.MainApiRepository
@@ -21,6 +22,9 @@ class LoginViewModel @Inject constructor(
     private val _userData = MutableStateFlow<BaseResponse<UserResponse>>(BaseResponse())
     var userData: StateFlow<BaseResponse<UserResponse>> = _userData
 
+    private val _loginData = MutableStateFlow<BaseResponse<String>>(BaseResponse())
+    var loginData : StateFlow<BaseResponse<String>> = _loginData
+
     fun postSignUpInfo(userDto: UserDto) {
         viewModelScope.launch {
             try {
@@ -29,6 +33,18 @@ class LoginViewModel @Inject constructor(
                 }
             } catch (e: Exception) {
                 Log.e("SignUp", "Error")
+            }
+        }
+    }
+
+    fun postUserLogInInfo(logInDto: LogInDto){
+        viewModelScope.launch {
+            try {
+                mainApiRepository.postLogInInfo(logInDto).collect {
+                    _loginData.value = it
+                }
+            }catch (e: Exception){
+                Log.e("Login","Error")
             }
         }
     }
