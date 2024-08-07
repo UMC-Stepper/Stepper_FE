23458d3a75384e5730577a038482a0754b475a61
김성민 <arcn1345@gmail.com>
2024-08-07 23:41:15 +0900

    feat: 투데이 홈 운동카드 리사이클러뷰 어댑터 추가

---
+++ app/src/main/java/com/example/umc_stepper/ui/today/home/TodayHomeExerciseCardAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.umc_stepper.ui.today.home
+
+import android.util.Log
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseAdapter
+import com.example.umc_stepper.base.BaseDiffCallback
+import com.example.umc_stepper.databinding.ItemTodayHomeExerciseCardBinding
+import com.example.umc_stepper.domain.model.local.ExerciseState
+
+class TodayHomeExerciseCardAdapter: BaseAdapter<ExerciseState, ItemTodayHomeExerciseCardBinding> (
+    diffCallback = BaseDiffCallback(
+        itemsTheSame = { oldItem, newItem -> oldItem == newItem },
+        contentsTheSame = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
+
+    override val layoutId: Int get() = R.layout.item_today_home_exercise_card
+
+    override fun bind(binding: ItemTodayHomeExerciseCardBinding, item: ExerciseState) {
+        binding.exerciseState = item
+    }
+
+}
