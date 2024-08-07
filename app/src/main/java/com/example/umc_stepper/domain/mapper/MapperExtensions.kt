547c03e75c916f910610ad139fe5e8ef6318208b
김성민 <arcn1345@gmail.com>
2024-08-07 23:40:05 +0900

    feat: 운동 카드 dto -> TodayExerciseState mapper 확장 함수 추가

---
+++ app/src/main/java/com/example/umc_stepper/domain/mapper/MapperExtensions.kt
@@ -0,0 +1,22 @@
+package com.example.umc_stepper.domain.mapper
+
+import com.example.umc_stepper.base.BaseListResponse
+import com.example.umc_stepper.domain.model.local.ExerciseState
+import com.example.umc_stepper.domain.model.local.ExerciseStep
+import com.example.umc_stepper.domain.model.response.ToDayExerciseResponseDto
+
+fun BaseListResponse<ToDayExerciseResponseDto>.toExerciseStates(): List<ExerciseState> {
+    return this.result?.map { dto ->
+        val steps = dto.stepList?.map {
+            ExerciseStep(step = it.step, stepStatus = it.step_status)
+        } ?: emptyList()
+
+        ExerciseState(
+            bodyPart = dto.bodyPart,
+            steps = steps,
+            isSuccess = this.isSuccess,
+            code = this.code,
+            message = this.message
+        )
+    } ?: emptyList()
+}
