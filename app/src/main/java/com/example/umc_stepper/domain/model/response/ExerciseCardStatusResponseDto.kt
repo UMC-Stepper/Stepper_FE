0c505580c765d5ee1d323383837ae7ece79f721a
김성민 <arcn1345@gmail.com>
2024-08-08 17:36:05 +0900

    reactor: ExerciseCardStatusResponseDto 변수명 변경

--- app/src/main/java/com/example/umc_stepper/domain/model/response/ExerciseCardStatusResponseDto.kt
+++ app/src/main/java/com/example/umc_stepper/domain/model/response/ExerciseCardStatusResponseDto.kt
@@ -3,7 +3,7 @@ package com.example.umc_stepper.domain.model.response
 import com.example.umc_stepper.utils.enums.bodyPartType
 
 data class ExerciseCardStatusResponseDto(
-    val date:String ="",
+    val date: String = "",
     val status: Boolean = false,
-    val bodyPartType: bodyPartType
+    val bodyPart: bodyPartType
 )
