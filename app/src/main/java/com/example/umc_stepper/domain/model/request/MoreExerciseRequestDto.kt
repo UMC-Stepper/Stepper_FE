a7e2f5ad76d7e78744a114c61076eba3f78f7955
김성민 <arcn1345@gmail.com>
2024-08-08 22:15:00 +0900

    feat: MoreExerciseResponseDto, MoreExerciseRequestDto 추가

---
+++ app/src/main/java/com/example/umc_stepper/domain/model/request/MoreExerciseRequestDto.kt
@@ -0,0 +1,7 @@
+package com.example.umc_stepper.domain.model.request
+
+data class MoreExerciseRequestDto(
+    val hour: Int = 0,
+    val minutes: Int = 0,
+    val seconds: Int = 0
+)
---
+++ app/src/main/java/com/example/umc_stepper/domain/model/response/MoreExerciseResponseDto.kt
@@ -0,0 +1,9 @@
+package com.example.umc_stepper.domain.model.response
+
+data class MoreExerciseResponseDto(
+    val id: Int = 0,
+    val date: String = "",
+    val hour: Int = 0,
+    val minutes: Int = 0,
+    val seconds: Int = 0
+)
