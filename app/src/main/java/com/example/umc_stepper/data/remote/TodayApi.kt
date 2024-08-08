e38012aa47ecbc865e7917a9bb0c235a3abd67f3
김성민 <arcn1345@gmail.com>
2024-08-08 15:24:18 +0900

    feat : TodayApi에 월별 운동 카드 상태 조회 API 추가

--- app/src/main/java/com/example/umc_stepper/data/remote/TodayApi.kt
+++ app/src/main/java/com/example/umc_stepper/data/remote/TodayApi.kt
@@ -3,6 +3,7 @@ package com.example.umc_stepper.data.remote
 import com.example.umc_stepper.base.BaseListResponse
 import com.example.umc_stepper.base.BaseResponse
 import com.example.umc_stepper.domain.model.response.CheckExerciseResponseDTO
+import com.example.umc_stepper.domain.model.response.ExerciseCardStatusResponseDto
 import com.example.umc_stepper.domain.model.response.ExerciseCardWeekResponseDto
 import com.example.umc_stepper.domain.model.response.ToDayExerciseResponseDto
 import retrofit2.http.GET
@@ -25,6 +26,12 @@ interface TodayApi {
     suspend fun getExerciseCheckDate(
         @Query("bodyPart") bodyPart: String
     ) : BaseListResponse<ExerciseCardWeekResponseDto>
+
+    // 월별 운동 카드 상태 조회 API
+    @GET("/api/exercise-card")
+    suspend fun getExerciseMonthCheck(
+        @Query("month") month: Int
+    ) : BaseListResponse<ExerciseCardStatusResponseDto>
 
     // 나만의 운동
 
