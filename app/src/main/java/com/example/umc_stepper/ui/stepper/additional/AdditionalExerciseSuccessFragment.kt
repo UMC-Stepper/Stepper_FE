ae4dd481a495821253fa75bc62376216deff1a42
김성민 <arcn1345@gmail.com>
2024-08-08 23:41:35 +0900

    feat: 타이머 운동 완료 화면으로 보내기

--- app/src/main/java/com/example/umc_stepper/ui/stepper/additional/AdditionalExerciseSuccessFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/additional/AdditionalExerciseSuccessFragment.kt
@@ -1,14 +1,35 @@
 package com.example.umc_stepper.ui.stepper.additional
 
+import android.util.Log
 import androidx.navigation.fragment.findNavController
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
 import com.example.umc_stepper.databinding.FragmentTodayExerciseSuccessBinding
+import com.example.umc_stepper.domain.model.Time
+import com.example.umc_stepper.domain.model.response.RateDiaryResponse
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class AdditionalExerciseSuccessFragment : BaseFragment<FragmentTodayExerciseSuccessBinding>(R.layout.fragment_today_exercise_success) {
+
     override fun setLayout() {
         setTitle()
         setOnClickBtn()
+
+        // 받는 부분
+        val args = arguments
+        val timeJson = args?.getString("time")
+
+        Log.d("EvaluationExerciseTodayFragment11111", "timeJson: $timeJson")
+
+        if (timeJson != null) {
+            val gson = Gson()
+            val time: Time = gson.fromJson(timeJson, Time::class.java)
+
+            // 받은 데이터 사용
+            Log.d("AdditionalExerciseSuccessFragment3333", "time: $time")
+        }
+
     }
 
     private fun setTitle() {
