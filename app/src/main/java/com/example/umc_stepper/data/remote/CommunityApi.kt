b780dc8329f685ecf497708e3b686b077ec77a49
Lee Chaeyoung <0929alice@naver.com>
2024-08-09 16:21:22 +0900

    feat: 내가 작성한 댓글조회, 댓글 작성, 댓글조회 interface

--- app/src/main/java/com/example/umc_stepper/data/remote/CommunityApi.kt
+++ app/src/main/java/com/example/umc_stepper/data/remote/CommunityApi.kt
@@ -2,7 +2,12 @@ package com.example.umc_stepper.data.remote
 
 import com.example.umc_stepper.base.BaseResponse
 import com.example.umc_stepper.domain.model.request.AiVideoDto
+import com.example.umc_stepper.domain.model.request.comment_controller.CommentWriteDto
+import com.example.umc_stepper.domain.model.request.member_controller.LogInDto
 import com.example.umc_stepper.domain.model.response.AiVideoInfo
+import com.example.umc_stepper.domain.model.response.comment_controller.CommentResponse
+import com.example.umc_stepper.domain.model.response.comment_controller.CommentWriteResponse
+import com.example.umc_stepper.domain.model.response.post_controller.CommunityMyCommentsResponse
 import com.example.umc_stepper.domain.model.response.post_controller.ScrapResponse
 import com.example.umc_stepper.domain.model.response.post_controller.CommunityMyPostsResponse
 import com.google.android.gms.common.internal.safeparcel.SafeParcelable.Param
@@ -10,6 +15,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CommunityApi {
     //스크랩 등록
@@ -25,5 +31,23 @@ interface CommunityApi {
     suspend fun getCommunityMyPosts(
 
     ):BaseResponse<CommunityMyPostsResponse>
+
+    //내가 작성한 댓글 조회
+    @GET("/api/community/my_comments")
+    suspend fun getCommunityMyComments(
+
+    ):BaseResponse<CommunityMyCommentsResponse>
+
+    //댓글 작성
+    @POST("/api/comment/write")
+    suspend fun postCommentWrite(
+        @Body commentWriteDto: CommentWriteDto
+    ):BaseResponse<CommentWriteResponse>
+
+    //댓글 조회
+    @GET("api/comment/{postId}/comment")
+    suspend fun  getComment(
+        @Path("postId") postId : Int
+    ):BaseResponse<CommentResponse>
 
 }
