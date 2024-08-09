cf3ab5280c1d4c71076de3f7428b321e196e5feb
Lee Chaeyoung <0929alice@naver.com>
2024-08-09 21:51:23 +0900

    feat: 뱃지 api 연결 중간저장

--- app/src/main/java/com/example/umc_stepper/ui/badge/BadgeFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/badge/BadgeFragment.kt
@@ -1,26 +1,61 @@
 package com.example.umc_stepper.ui.badge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
 import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseFragment
+import com.example.umc_stepper.databinding.FragmentBadgeBinding
+import com.example.umc_stepper.domain.model.response.Badge
+import com.example.umc_stepper.ui.login.MainViewModel
+import com.example.umc_stepper.ui.stepper.StepperViewModel
+import kotlinx.coroutines.launch
 
-class BadgeFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+class BadgeFragment : BaseFragment<FragmentBadgeBinding>(R.layout.fragment_badge) {
+    private val mainViewModel: MainViewModel by activityViewModels()
+    override fun setLayout() {
+        getBadge()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_badge, container, false)
+    private fun getBadge() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.getBadge()
+            }
+            }
     }
 
+    private fun setBadge(){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                try {
+                    // response -> BaseListResponse
+                    // response.result -> badgeResponseItem
+                    // badge -> badgeResponseItem.list
+                    mainViewModel.getBadge.collect { response ->
+                        val badgeResponseItem = response.result
+                        val badge = Badge()
+                        badgeResponseItem?.forEach { i ->
+                            binding.fragmentBadgeYellow1Tv.text = i.categoryName
+                            Log.e("mainViewModel ","it : ${i.categoryName}")
+                            i.list.forEach { j ->
+                                j.id
+                            }
+
+                        }
+                    }
+                } catch (e:Exception) {
+
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/umc_stepper/ui/login/MainViewModel.kt
+++ app/src/main/java/com/example/umc_stepper/ui/login/MainViewModel.kt
@@ -1,13 +1,40 @@
-//package com.example.umc_stepper.ui.login
-//
-//import androidx.lifecycle.ViewModel
-//import com.example.umc_stepper.domain.repository.MainApiRepository
-//import dagger.hilt.android.lifecycle.HiltViewModel
-//import javax.inject.Inject
-//
-//@HiltViewModel
-//class MainViewModel @Inject constructor(
-//    private val mainApiRepository: MainApiRepository
-//) : ViewModel(){
-//
-//}
+package com.example.umc_stepper.ui.login
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.umc_stepper.base.BaseListResponse
+import com.example.umc_stepper.base.BaseResponse
+import com.example.umc_stepper.domain.model.response.BadgeResponseItem
+import com.example.umc_stepper.domain.repository.MainApiRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val mainApiRepository: MainApiRepository
+) : ViewModel(){
+
+    private val _getBadge = MutableStateFlow<BaseListResponse<BadgeResponseItem>>(BaseListResponse())
+    val getBadge : StateFlow<BaseListResponse<BadgeResponseItem>> = _getBadge
+
+    fun getBadge(){
+        viewModelScope.launch {
+            try{
+                mainApiRepository.getBadge().collect{
+                    _getBadge.value = it
+                    //Log.e("mainViewModel ","it : $it")
+                }
+            }catch(e:Exception){
+                Log.e("mainViewModel getBadge",e.message.toString())
+            }
+
+        }
+    }
+
+    private val TAG = "MainViewModel"
+
+}
