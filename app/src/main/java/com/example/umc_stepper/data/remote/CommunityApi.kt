03291c921a4b629453c69e057eced4bb8468ff27
Lee Chaeyoung <0929alice@naver.com>
2024-08-09 16:08:45 +0900

    feat: 내가 작성한 글 조회 interface

--- app/src/main/java/com/example/umc_stepper/data/remote/CommunityApi.kt
+++ app/src/main/java/com/example/umc_stepper/data/remote/CommunityApi.kt
@@ -1,16 +1,25 @@
 package com.example.umc_stepper.data.remote
 
+import com.example.umc_stepper.base.BaseResponse
 import com.example.umc_stepper.domain.model.request.AiVideoDto
 import com.example.umc_stepper.domain.model.response.AiVideoInfo
+import com.example.umc_stepper.domain.model.response.post_controller.CommunityMyPostsResponse
 import com.google.android.gms.common.internal.safeparcel.SafeParcelable.Param
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface CommunityApi {
-//스크랩 등록
+    //스크랩 등록
     @POST("/api/community/{postId}/scrap")
     fun postCommitScrap(
         @Param()
     )
+
+    //내가 작성한 글 조회
+    @GET("/api/community/my_posts")
+    suspend fun getCommunityMyPosts(
+
+    ):BaseResponse<CommunityMyPostsResponse>
 
 }
