2fc0e71ad7fece751112d8ee1d01d15d7df3252a
Lee Chaeyoung <0929alice@naver.com>
2024-08-09 20:07:10 +0900

    refactor: 에러잡기, additional_exercise_youtube2.xml padding값

--- app/src/main/java/com/example/umc_stepper/ui/community/CommunityViewModel.kt
+++ app/src/main/java/com/example/umc_stepper/ui/community/CommunityViewModel.kt
@@ -1,11 +1,11 @@
-package com.example.umc_stepper.ui.community
-
-import androidx.lifecycle.ViewModel
-import com.example.umc_stepper.domain.repository.CommunityApiRepository
-import javax.inject.Inject
-
-class CommunityViewModel @Inject constructor(
-    private val communityApiRepository: CommunityApiRepository
-) : ViewModel() {
-
-}
+//package com.example.umc_stepper.ui.community
+//
+//import androidx.lifecycle.ViewModel
+//import com.example.umc_stepper.domain.repository.CommunityApiRepository
+//import javax.inject.Inject
+//
+//class CommunityViewModel @Inject constructor(
+//    private val communityApiRepository: CommunityApiRepository
+//) : ViewModel() {
+//
+//}
--- app/src/main/java/com/example/umc_stepper/ui/login/MainViewModel.kt
+++ app/src/main/java/com/example/umc_stepper/ui/login/MainViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.umc_stepper.ui.login
-
-import androidx.lifecycle.ViewModel
-import com.example.umc_stepper.domain.repository.MainApiRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class MainViewModel @Inject constructor(
-    private val mainApiRepository: MainApiRepository
-) : ViewModel(){
-    
-}
+//package com.example.umc_stepper.ui.login
+//
+//import androidx.lifecycle.ViewModel
+//import com.example.umc_stepper.domain.repository.MainApiRepository
+//import dagger.hilt.android.lifecycle.HiltViewModel
+//import javax.inject.Inject
+//
+//@HiltViewModel
+//class MainViewModel @Inject constructor(
+//    private val mainApiRepository: MainApiRepository
+//) : ViewModel(){
+//
+//}
