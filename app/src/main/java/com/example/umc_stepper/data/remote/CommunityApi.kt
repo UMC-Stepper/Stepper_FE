93bd7513ea745ae3f579b4a1c117807404debb27
jiwon <pjw99032986@gmail.com>
2024-08-09 16:07:29 +0900

    feat : data class response , request 추가

--- app/src/main/java/com/example/umc_stepper/data/remote/CommunityApi.kt
+++ app/src/main/java/com/example/umc_stepper/data/remote/CommunityApi.kt
@@ -1,16 +1,21 @@
 package com.example.umc_stepper.data.remote
 
+import com.example.umc_stepper.base.BaseResponse
 import com.example.umc_stepper.domain.model.request.AiVideoDto
 import com.example.umc_stepper.domain.model.response.AiVideoInfo
+import com.example.umc_stepper.domain.model.response.post_controller.ScrapResponse
 import com.google.android.gms.common.internal.safeparcel.SafeParcelable.Param
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface CommunityApi {
 //스크랩 등록
     @POST("/api/community/{postId}/scrap")
-    fun postCommitScrap(
-        @Param()
-    )
+    suspend fun postCommitScrap(
+        @Path("postId") postId : Int
+    ) : BaseResponse<ScrapResponse>
+
+
 
 }
