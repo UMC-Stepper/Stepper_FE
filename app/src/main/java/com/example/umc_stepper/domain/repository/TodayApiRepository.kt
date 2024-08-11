2bc3c21f9ee7af3a6f4efd6f6a19030f04bd6992
김성민 <arcn1345@gmail.com>
2024-08-11 19:03:11 +0900

    reactor : TodayApiRepository 주석 변경

--- app/src/main/java/com/example/umc_stepper/domain/repository/TodayApiRepository.kt
+++ app/src/main/java/com/example/umc_stepper/domain/repository/TodayApiRepository.kt
@@ -17,8 +17,8 @@ interface TodayApiRepository {
     suspend fun postAddExerciseCard(exerciseCardRequestDto: ExerciseCardRequestDto)	: Flow<BaseResponse<ExerciseCardResponse>>// 운동 카드 추가 API
     suspend fun postInquiryExerciseCard(exerciseId: Int) : Flow<BaseResponse<ExerciseCardResponse>>      // 운동 카드 상세 조회 API
     suspend fun getTodayExerciseState(date: String) : Flow<BaseListResponse<ToDayExerciseResponseDto>>  // 오늘의 운동 진행 상태 조회
-    suspend fun getMyExercise(bodyPart: String) : Flow<BaseListResponse<CheckExerciseResponse>>     // 나만의 운동 조회
+    suspend fun getMyExercise(bodyPart: String) : Flow<BaseListResponse<CheckExerciseResponse>>     // 나만의 운동 조회 -> 스크랩 화면
     suspend fun getExerciseMonthCheck(month: Int) : Flow<BaseListResponse<ExerciseCardStatusResponseDto>> // 월별 운동 카드 상태 조회 API
     suspend fun postAddMyExercise(addExerciseRequestDto: ExerciseCardRequestDto) : Flow<BaseResponse<AddExerciseResponse>>  // 나만의 운동 추가 API
-    suspend fun getExerciseCheckDate(bodyPart: String) : Flow<BaseListResponse<ExerciseCardWeekResponseDto>> // 나만의 운동 조회 -> 스크랩 화면
+    suspend fun getExerciseCheckDate(bodyPart: String) : Flow<BaseListResponse<ExerciseCardWeekResponseDto>> // 운동 부위의 운동 카드 요일 조회
 }
