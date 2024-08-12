a2972ac4715df868c56ebf4f5014047f53b5f785
김성민 <arcn1345@gmail.com>
2024-08-13 03:52:18 +0900

    feat: 위클리 게시글 상세 보기 프래그먼트 postId 받아서 보여줌

--- app/src/main/java/com/example/umc_stepper/ui/community/weekly/CommunityWeeklyShowPostFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/community/weekly/CommunityWeeklyShowPostFragment.kt
@@ -21,12 +21,14 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Locale
+import kotlin.properties.Delegates
 
 class CommunityWeeklyShowPostFragment : BaseFragment<FragmentCommunityWeeklyShowPostBinding>(R.layout.fragment_community_weekly_show_post) {
 
     private lateinit var mainActivity: MainActivity
     private lateinit var weeklyShowPostImageAdapter: WeeklyShowPostImageAdapter
     private lateinit var weeklyShowPostReplyAdapter: WeeklyShowPostReplyAdapter
+    private var postId by Delegates.notNull<Int>()
     private val communityViewModel: CommunityViewModel by activityViewModels()
 
     override fun onAttach(context: Context) {
@@ -36,9 +38,10 @@ class CommunityWeeklyShowPostFragment : BaseFragment<FragmentCommunityWeeklyShow
 
     override fun setLayout() {
         updateMainToolbar()
+        setPostId()
         setAdapter()
         observeViewModel()
-        setImageView()
+        onClickImageView()
     }
 
     private fun setAdapter() {
@@ -49,19 +52,24 @@ class CommunityWeeklyShowPostFragment : BaseFragment<FragmentCommunityWeeklyShow
         binding.fragmentCommunityWeeklyShowPostReplyRv.adapter = weeklyShowPostReplyAdapter
     }
 
-    private fun setImageView() {
+    private fun onClickImageView() {
 
         // 좋아요 등록/취소
         binding.fragmentCommunityWeeklyShowThumbsUpIv.setOnClickListener {
             viewLifecycleOwner.lifecycleScope.launch {
                 repeatOnLifecycle(Lifecycle.State.STARTED) {
                     launch {
-                        if (communityViewModel.isLike.value) {
-                            communityViewModel.deleteCancelLike(3)
-                            binding.fragmentCommunityWeeklyShowThumbsUpIv.setImageResource(R.drawable.ic_thumbs_up)
+                        val currentState = communityViewModel.isLike.value
+                        if (currentState) {
+                            communityViewModel.deleteCancelLike(postId)
                         } else {
-                            communityViewModel.postLikeEdit(3)
-                            binding.fragmentCommunityWeeklyShowThumbsUpIv.setImageResource(R.drawable.ic_thumbs_up_fill)
+                            communityViewModel.postLikeEdit(postId)
+                        }
+                        communityViewModel.isLike.collectLatest { isLiked ->
+                            binding.fragmentCommunityWeeklyShowThumbsUpIv.setImageResource(
+                                if (isLiked) R.drawable.ic_thumbs_up_fill
+                                else R.drawable.ic_thumbs_up
+                            )
                         }
                     }
                 }
@@ -72,15 +80,26 @@ class CommunityWeeklyShowPostFragment : BaseFragment<FragmentCommunityWeeklyShow
         binding.fragmentCommunityWeeklyShowScrapsUpIv.setOnClickListener {
             viewLifecycleOwner.lifecycleScope.launch {
                 repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    if (communityViewModel.isScrap.value) {
-                        communityViewModel.deleteCancelScrap(3)
-                        binding.fragmentCommunityWeeklyShowScrapsUpIv.setImageResource(R.drawable.ic_scraps_up)
+                    val currentState = communityViewModel.isScrap.value
+                    if (currentState) {
+                        communityViewModel.deleteCancelScrap(postId)
                     } else {
-                        communityViewModel.postCommitScrap(3)
-                        binding.fragmentCommunityWeeklyShowScrapsUpIv.setImageResource(R.drawable.ic_scrap_fill)
+                        communityViewModel.postCommitScrap(postId)
+                    }
+                    communityViewModel.isLike.collectLatest { isLiked ->
+                        binding.fragmentCommunityWeeklyShowScrapsUpIv.setImageResource(
+                            if (isLiked) R.drawable. ic_scrap_fill
+                            else R.drawable.ic_scraps_up
+                        )
                     }
                 }
             }
+        }
+    }
+
+    private fun setPostId() {
+        arguments?.let {
+            postId = it.getString("postId")?.toInt() ?: 1
         }
     }
 
@@ -90,36 +109,24 @@ class CommunityWeeklyShowPostFragment : BaseFragment<FragmentCommunityWeeklyShow
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
-                    communityViewModel.getDetailPost(3)
+                    communityViewModel.getDetailPost(postId.toInt())
                 }
 
                 launch {
                     communityViewModel.apiResponsePostViewResponse.collectLatest {
-                        if(it.isSuccess) {
+                        if (it.isSuccess) {
                             binding.apiResponsePostViewResponse = it.result
 
                             // 입출력 데이터 포맷 설정
-                            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
-                            val outputFormat = SimpleDateFormat("yy.MM.dd  HH:mm", Locale.getDefault())
+                            val inputFormat =
+                                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
+                            val outputFormat =
+                                SimpleDateFormat("yy.MM.dd  HH:mm", Locale.getDefault())
 
                             // 날짜 파싱 및 변환
                             val date = inputFormat.parse(it.result?.updatedAt)
                             binding.fragmentCommunityWeeklyShowPostDateTv.text = date?.let { it -> outputFormat.format(it) }
                         }
-                    }
-                }
-
-                // 좋아요, 스크랩 이미지 설정
-                launch {
-                    if(communityViewModel.isLike.value) {
-                        binding.fragmentCommunityWeeklyShowThumbsUpIv.setImageResource(R.drawable.ic_thumbs_up_fill)
-                    } else {
-                        binding.fragmentCommunityWeeklyShowThumbsUpIv.setImageResource(R.drawable.ic_thumbs_up)
-                    }
-                    if(communityViewModel.isScrap.value) {
-                        binding.fragmentCommunityWeeklyShowScrapsUpIv.setImageResource(R.drawable.ic_scrap_fill)
-                    } else {
-                        binding.fragmentCommunityWeeklyShowScrapsUpIv.setImageResource(R.drawable.ic_scraps_up)
                     }
                 }
             }
