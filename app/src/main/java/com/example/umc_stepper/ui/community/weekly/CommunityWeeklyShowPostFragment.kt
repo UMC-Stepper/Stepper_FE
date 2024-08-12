03f7ed2162dca735806b863ce1e9a284fd12edaf
김성민 <arcn1345@gmail.com>
2024-08-13 01:01:41 +0900

    feat : 상세 글 보기 날짜 파싱하여 설정

--- app/src/main/java/com/example/umc_stepper/ui/community/weekly/CommunityWeeklyShowPostFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/community/weekly/CommunityWeeklyShowPostFragment.kt
@@ -2,18 +2,32 @@ package com.example.umc_stepper.ui.community.weekly
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
 import com.example.umc_stepper.databinding.FragmentCommunityWeeklyShowPostBinding
 import com.example.umc_stepper.ui.MainActivity
+import com.example.umc_stepper.ui.community.CommunityViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CommunityWeeklyShowPostFragment : BaseFragment<FragmentCommunityWeeklyShowPostBinding>(R.layout.fragment_community_weekly_show_post) {
 
     private lateinit var mainActivity: MainActivity
+    private lateinit var weeklyShowPostImageAdapter: WeeklyShowPostImageAdapter
+    private lateinit var weeklyShowPostReplyAdapter: WeeklyShowPostReplyAdapter
+    private val communityViewModel: CommunityViewModel by activityViewModels()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -22,6 +36,56 @@ class CommunityWeeklyShowPostFragment : BaseFragment<FragmentCommunityWeeklyShow
 
     override fun setLayout() {
         updateMainToolbar()
+        setAdapter()
+        observeViewModel()
+    }
+
+    private fun setAdapter() {
+        weeklyShowPostImageAdapter = WeeklyShowPostImageAdapter()
+        binding.fragmentCommunityWeeklyShowPostImgRv.adapter = weeklyShowPostImageAdapter
+
+        weeklyShowPostReplyAdapter = WeeklyShowPostReplyAdapter()
+        binding.fragmentCommunityWeeklyShowPostReplyRv.adapter = weeklyShowPostReplyAdapter
+    }
+
+    private fun observeViewModel() {
+
+        // 게시글 상세 조회
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    communityViewModel.getDetailPost(3)
+                }
+
+                launch {
+                    communityViewModel.apiResponsePostViewResponse.collectLatest {
+                        if(it.isSuccess) {
+                            binding.apiResponsePostViewResponse = it.result
+
+                            // 입출력 데이터 포맷 설정
+                            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
+                            val outputFormat = SimpleDateFormat("dd.MM.yy HH:mm", Locale.getDefault())
+
+                            // 날짜 파싱 및 변환
+                            val date = inputFormat.parse(it.result?.updatedAt)
+                            binding.fragmentCommunityWeeklyShowPostDateTv.text = date?.let { it -> outputFormat.format(it) }
+                        }
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+            }
+        }
     }
 
     private fun updateMainToolbar() {
