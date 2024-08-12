599be7381f7dda215c25b6a3efe62b92ddb4a6ad
김성민 <arcn1345@gmail.com>
2024-08-12 13:58:39 +0900

    feat : 내가 작성한 댓글 글 목록 어댑터 추가

---
+++ app/src/main/java/com/example/umc_stepper/ui/community/savedcontents/comments/MyCommentsAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.umc_stepper.ui.community.savedcontents.comments
+
+import android.util.Log
+import com.example.umc_stepper.R
+import com.example.umc_stepper.base.BaseAdapter
+import com.example.umc_stepper.base.BaseDiffCallback
+import com.example.umc_stepper.databinding.ItemCommunityWeeklyHomePostBinding
+import com.example.umc_stepper.domain.model.response.post_controller.CommunityMyCommentsResponseItem
+import com.example.umc_stepper.utils.listener.ItemClickListener
+
+class MyCommentsAdapter(onItemClick: ItemClickListener) :
+    BaseAdapter<CommunityMyCommentsResponseItem, ItemCommunityWeeklyHomePostBinding>(
+        diffCallback = BaseDiffCallback(
+            itemsTheSame = { oldItem, newItem -> oldItem.id == newItem.id },
+            contentsTheSame = { oldItem, newItem -> oldItem == newItem }
+        )
+    ) {
+
+    override val layoutId: Int
+        get() = R.layout.item_community_weekly_home_post
+
+    override fun bind(
+        binding: ItemCommunityWeeklyHomePostBinding,
+        item: CommunityMyCommentsResponseItem
+    ) {
+        Log.d("MyCommentsAdapter","$item")
+        binding.communityMyCommentsResponseItem = item
+    }
+
+}
