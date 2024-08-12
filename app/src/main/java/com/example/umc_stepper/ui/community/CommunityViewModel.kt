778918ecc1e10b431b22a5de61c730fb75645cc1
김성민 <arcn1345@gmail.com>
2024-08-12 13:58:15 +0900

    feat : 커뮤니티 뷰모델 작성 댓글의 게시물 조회 추가

--- app/src/main/java/com/example/umc_stepper/ui/community/CommunityViewModel.kt
+++ app/src/main/java/com/example/umc_stepper/ui/community/CommunityViewModel.kt
@@ -1,11 +1,40 @@
-//package com.example.umc_stepper.ui.community
-//
-//import androidx.lifecycle.ViewModel
-//import com.example.umc_stepper.domain.repository.CommunityApiRepository
-//import javax.inject.Inject
-//
-//class CommunityViewModel @Inject constructor(
-//    private val communityApiRepository: CommunityApiRepository
-//) : ViewModel() {
-//
-//}
+package com.example.umc_stepper.ui.community
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.umc_stepper.base.BaseListResponse
+import com.example.umc_stepper.base.BaseResponse
+import com.example.umc_stepper.domain.model.response.post_controller.CommunityMyCommentsResponseItem
+import com.example.umc_stepper.domain.repository.CommunityApiRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CommunityViewModel @Inject constructor(
+    private val communityApiRepository: CommunityApiRepository
+) : ViewModel() {
+
+    // 내가 작성한 댓글의 게시글 조회
+    private val _communityMyCommentsResponseItem = MutableStateFlow<BaseListResponse<CommunityMyCommentsResponseItem>>(BaseListResponse())
+    val communityMyCommentsResponseItem : StateFlow<BaseListResponse<CommunityMyCommentsResponseItem>> = _communityMyCommentsResponseItem
+
+
+    // 내가 작성한 댓글의 게시글 조회
+    fun getCommunityMyComments() {
+        viewModelScope.launch {
+            try{
+                communityApiRepository.getCommunityMyComments().collect {
+                    _communityMyCommentsResponseItem.value = it
+                    Log.d("CommunityViewModel", "CommunityMyCommentsResponseItem : $it")
+                }
+            } catch (e:Exception) {
+                Log.e("getCommunityMyComments is Error", e.message.toString())
+            }
+        }
+    }
+
+}
