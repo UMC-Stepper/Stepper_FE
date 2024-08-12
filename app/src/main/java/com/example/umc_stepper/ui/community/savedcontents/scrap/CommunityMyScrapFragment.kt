12d64e715efdb2f269bb9d84fb0e61928ad20880
김성민 <arcn1345@gmail.com>
2024-08-13 02:54:02 +0900

    feat : 내가 스크랩 한 글 목록 프래그먼트 어댑터 설정

--- app/src/main/java/com/example/umc_stepper/ui/community/savedcontents/scrap/CommunityMyScrapFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/community/savedcontents/scrap/CommunityMyScrapFragment.kt
@@ -1,14 +1,24 @@
 package com.example.umc_stepper.ui.community.savedcontents.scrap
 
 import android.content.Context
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
 import com.example.umc_stepper.databinding.FragmentCommunityMyScrapBinding
 import com.example.umc_stepper.ui.MainActivity
+import com.example.umc_stepper.ui.community.CommunityViewModel
+import com.example.umc_stepper.utils.listener.ItemClickListener
+import kotlinx.coroutines.launch
 
-class CommunityMyScrapFragment : BaseFragment<FragmentCommunityMyScrapBinding>(R.layout.fragment_community_my_scrap) {
+class CommunityMyScrapFragment : BaseFragment<FragmentCommunityMyScrapBinding>(R.layout.fragment_community_my_scrap),
+    ItemClickListener {
 
     private lateinit var mainActivity: MainActivity
+    private lateinit var myScrapAdapter: MyScrapAdapter
+    private val communityViewModel: CommunityViewModel by activityViewModels()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -17,6 +27,20 @@ class CommunityMyScrapFragment : BaseFragment<FragmentCommunityMyScrapBinding>(R
 
     override fun setLayout() {
         updateToolbar()
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        myScrapAdapter = MyScrapAdapter(this)
+        binding.fragmentCommunityMyScrapRv.adapter = myScrapAdapter
+    }
+
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // 내가 스크랩 한 글 목록 불러오기
+            }
+        }
     }
 
     private fun updateToolbar() {
@@ -24,6 +48,10 @@ class CommunityMyScrapFragment : BaseFragment<FragmentCommunityMyScrapBinding>(R
         mainActivity.updateToolbarLeftImg(R.drawable.ic_toolbar_community_home)
         mainActivity.updateToolbarMiddleImg(R.drawable.ic_toolbar_community_search)
         mainActivity.updateToolbarRightImg(R.drawable.ic_toolbar_community_menu)
+    }
+
+    override fun onClick(item: Any) {
+        TODO("Not yet implemented")
     }
 
 }
