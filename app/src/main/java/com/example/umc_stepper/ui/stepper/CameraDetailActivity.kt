b6e73aabb6b158c77588b40a1237c43f1959c67d
김성민 <arcn1345@gmail.com>
2024-08-13 22:11:55 +0900

    feat: CameraDetailActivity 사진 사용 버튼 누르면 평가일지 이미지뷰에 사진 들어가도록 함

--- app/src/main/java/com/example/umc_stepper/ui/stepper/CameraDetailActivity.kt
+++ app/src/main/java/com/example/umc_stepper/ui/stepper/CameraDetailActivity.kt
@@ -1,5 +1,6 @@
 package com.example.umc_stepper.ui.stepper
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -7,11 +8,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseActivity
 import com.example.umc_stepper.databinding.ActivityCameraDetailBinding
+import com.example.umc_stepper.ui.MainActivity
+import com.example.umc_stepper.utils.GlobalApplication
 
 class CameraDetailActivity : BaseActivity<ActivityCameraDetailBinding>(R.layout.activity_camera_detail) {
+
+    private var photoUri : Uri? = null
 
     override fun setLayout() {
         setImageView()
@@ -19,15 +25,14 @@ class CameraDetailActivity : BaseActivity<ActivityCameraDetailBinding>(R.layout.
     }
 
     private fun setImageView() {
-        val photoUri: Uri? = intent.getStringExtra("photo_uri")?.let { Uri.parse(it)}
-        Log.d("로그", "$photoUri")
+        photoUri = intent.getStringExtra("photo_uri")?.let { Uri.parse(it)}
+        Log.d("CameraDetailActivity", "photoUri : $photoUri")
         photoUri?.let {
-            binding.activityCameraDetailPhotoIv.setImageURI(it)
+            GlobalApplication.loadImage(binding.activityCameraDetailPhotoIv, it)
         }
     }
 
     private fun setButton() {
-
         // 다시 찍기 버튼
         binding.activityCameraDetailRetakeIv.setOnClickListener {
             startNextActivity(CameraActivity::class.java)
@@ -35,8 +40,17 @@ class CameraDetailActivity : BaseActivity<ActivityCameraDetailBinding>(R.layout.
 
         // 사진 사용 버튼
         binding.activityCameraDetailUsePhotoIv.setOnClickListener {
-
+            navigateToEvaluationExercise()
         }
+    }
+
+    private fun navigateToEvaluationExercise() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("navigate_to", "EvaluationExerciseFragment")
+            putExtra("photo_uri", photoUri.toString())
+        }
+        startActivity(intent)
+        finish()
     }
 
 }
