e2fb9f5d0789a2f5ab02eea2b5ba156aa2ddee35
김성민 <arcn1345@gmail.com>
2024-08-15 21:28:48 +0900

    feat : Firebase FCM 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
