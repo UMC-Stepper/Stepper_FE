d7089c00c1affae4ff0d630692b223b2e5f45657
김성민 <arcn1345@gmail.com>
2024-08-17 16:35:50 +0900

    feat: 설정 홈, 설정 유저정보 보기 화면

--- app/src/main/java/com/example/umc_stepper/ui/settings/SettingsShowProfileFragment.kt
+++ app/src/main/java/com/example/umc_stepper/ui/settings/SettingsShowProfileFragment.kt
@@ -1,19 +1,39 @@
 package com.example.umc_stepper.ui.settings
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.umc_stepper.R
 import com.example.umc_stepper.base.BaseFragment
 import com.example.umc_stepper.databinding.FragmentSettingsShowProfileBinding
+import com.example.umc_stepper.ui.login.LoginViewModel
+import kotlinx.coroutines.launch
 
 class SettingsShowProfileFragment : BaseFragment<FragmentSettingsShowProfileBinding>(R.layout.fragment_settings_show_profile) {
 
+    private val loginViewModel : LoginViewModel by activityViewModels()
+
     override fun setLayout() {
         setButton()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginViewModel.getUserInfo()
+                loginViewModel.userInfo.collect {
+                    privateGetUserInfo()
+                }
+            }
+        }
+    }
+
+    private suspend fun privateGetUserInfo() {
+        binding.userInfo = loginViewModel.userInfo.value.result
     }
 
     private fun setButton() {
