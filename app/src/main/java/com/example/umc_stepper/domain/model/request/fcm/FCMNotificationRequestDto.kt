96898bc3cc41d83041689ccb326a2368b5511d04
김성민 <arcn1345@gmail.com>
2024-08-20 15:45:37 +0900

    feat: FCM request dto

--- app/src/main/java/com/example/umc_stepper/domain/model/request/fcm/FCMNotificationRequestDto.kt
+++ app/src/main/java/com/example/umc_stepper/domain/model/request/fcm/FCMNotificationRequestDto.kt
@@ -1,4 +1,4 @@
-package com.example.umc_stepper.domain.model.request
+package com.example.umc_stepper.domain.model.request.fcm
 
 data class FCMNotificationRequestDto (
     val targetUserId: Int = 0,
