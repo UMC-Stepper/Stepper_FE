0c70eba2c4ab96351be3f78acbd13c6e9a2b1e27
김성민 <arcn1345@gmail.com>
2024-08-20 15:45:47 +0900

    reactor : ApiResponsePostResponse 수정

--- app/src/main/java/com/example/umc_stepper/domain/model/response/post_controller/ApiResponsePostResponse.kt
+++ app/src/main/java/com/example/umc_stepper/domain/model/response/post_controller/ApiResponsePostResponse.kt
@@ -1,14 +1,21 @@
 package com.example.umc_stepper.domain.model.response.post_controller
+
+import com.example.umc_stepper.domain.model.response.ImageResponse
+
 //사용자 게시글 작성 POST
 data class ApiResponsePostResponse(
-    val authorEmail: String = "",
-    val body: String = "",
-    val bodyPart: String? = "",
-    val createdAt: String = "",
     val id: Int = 0,
-    val imageUrl: String = "",
-    val subCategory: String? = "",
+    val profileImageUrl : String,
     val title: String = "",
+    val body: String = "",
+    val authorEmail: String = "",
+    val bodyPart: String? = "",
+    val likes: Int = 0,
+    val scraps: Int = 0,
+    val commentsCount: Int = 0,
+    val subCategory: String? = "",
+    val weeklyMissionTitle: String = "",
+    val createdAt: String = "",
     val updatedAt: String = "",
-    val weeklyMissionTitle: String = ""
+    val imageList: List<ImageResponse>
 )
